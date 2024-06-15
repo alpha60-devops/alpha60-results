6e88657937a405702bc16859b592effb2ce70210
Benjamin De Kosnik <bkoz@gnu.org>
2024-06-15 12:25:55 +1000

    btiha-size-bytes-histogram: use same gigabyte definition as alpha-60 summary file, aka 10 to the 9

--- scripts/btiha-size-bytes-histogram.py
+++ scripts/btiha-size-bytes-histogram.py
@@ -39,7 +39,8 @@ ofile = filename.with_suffix('.svg')
 df = pd.read_csv(ifile, header=None)
 
 # convert bytes to gigabytes
-kgb = 1024*1024*1024
+#kgb = 1024*1024*1024
+kgb = 1000*1000*1000
 dfgb = df.div(kgb)
 
 # results are series
